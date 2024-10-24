4c2c04d04c75ac6b86fc756a5cb930316cd6bd9e
payce06 <paycegrossman@gmail.com>
2024-10-23 22:37:21 -0400

    basic-cpp

---
+++ basic.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int num1 = 5;
+    float num2 = 3.14;
+    double num3 = 3.1415926;
+    char char1 = 'T';
+    string string1 = "Hello";
+    bool isBaseballTeam = true;
+    
+    const int num11 = 5;
+    const float num22 = 4.567;
+    const double num33 = 4.567938434;
+    const char char11 = 'S';
+    const string string11 = "Hola";
+    const bool isStudent = false;
+    
+    
+    
+    return 0;
+}
+
