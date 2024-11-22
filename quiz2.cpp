4a994bcdeb2d9e7309a2dee65e87f8c545f39b36
payce06 <paycegrossman@gmail.com>
2024-11-21 22:52:39 -0500

    quiz2-cpp

--- quiz2.cpp
+++ quiz2.cpp
@@ -10,3 +10,18 @@ int askMathQuestion() {
     // Generate random numbers and operator
     int num1 = rand() % 100 + 1; // Random number between 1 and 100
     int num2 = rand() % 100 + 1;
+    char operators[] = {'+', '-', '*', '/'};
+    char op = operators[rand() % 4]; // Randomly pick an operator
+
+    int correctAnswer = 0;
+
+    // Use if statements to determine the operation
+    if (op == '+') {
+        correctAnswer = num1 + num2;
+    } else if (op == '-') {
+        correctAnswer = num1 - num2;
+    } else if (op == '*') {
+        correctAnswer = num1 * num2;
+    } else if (op == '/') {
+        // Ensure no division by zero
+        while (num2 == 0) {
