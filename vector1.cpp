72631a8e8f7e3006962fa44915c124700b38c2cc
payce06 <paycegrossman@gmail.com>
2024-11-24 21:54:13 -0500

    vector1-cpp

--- vector1.cpp
+++ vector1.cpp
@@ -12,3 +12,19 @@ void printVector(vector<int> v1, string methodName) {
     }
     cout << endl;
 }
+
+int main()
+{
+    vector<int> vector1 = {1, 3, 2, 4};
+   
+    // front
+    // See the element at the 0 index
+    cout << "Front: " << vector1.front() << endl;
+   
+    // back
+    // See the element at the length - 1 index
+    cout << "Back: " << vector1.back() << endl;
+   
+    // empty
+    // Check if the vector is empty. Returns 1 for empty and 0 for non-empty
+    cout << "Is Empty: " << vector1.empty() << endl;
