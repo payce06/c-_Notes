eb45ee087169ca56140566b443bf63c1ae218a01
payce06 <paycegrossman@gmail.com>
2024-11-30 19:27:46 -0500

    standard_library.cpp

--- standard_library.cpp
+++ standard_library.cpp
@@ -36,3 +36,26 @@ int main() {
     vector<int> vec1 = {8, 5, 7, 3};
     sort(vec1.begin(), vec1.end());
     printVector(vec1);
+
+// 2. Size and empty check
+    cout << "Size: " << vec1.size() << endl;
+    cout << "Is Empty: " << (vec1.empty() ? "Yes" : "No");
+
+    // 3. Insert an element
+    vector<int> vec2 = {4, 6, 3};
+    vec2.insert(vec2.begin() +1, 10);
+    printVector(vec2);
+
+    // 4. Reverse a vector
+    vector<int> vec3 = {4, 2, 7, 3};
+    reverse(vec3.begin(), vec3.end());
+    printVector(vec3);
+
+    // 5. Remove the last element
+    vector<int> vec4 = {3, 6, 1};
+    vec4.pop_back();
+    printVector(vec4);
+
+    // 6. Create a vector of size 5 initialized to 100
+    vector<int> vec5 = {5, 100};
+    printVector(vec5);
