cfbe2cf0e752b451cf6f79e55453f6a754c52b8c
payce06 <paycegrossman@gmail.com>
2024-12-01 19:57:07 -0500

    inheritance-cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -19,4 +19,36 @@ int main(){
     Dog d;
     d.eating();
     d.barking();
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+class Animal{
+    public:
+    void eating(){
+        cout << "Eating..." << endl;
+    }
+};
+
+class Bird{
+    public:
+    void flying(){
+        cout << "Flying..." << endl;
+    }
+};
+
+class Duck : public Animal, public Bird{
+    public:
+    void quacking(){
+        cout << "Quacking..." << endl;
+    }
+};
+
+int main(){
+    Duck d;
+    d.eating();
+    d.flying();
+    d.quacking();
 }
