50f62d86c530ab5719ae5d4fa4dcd712c3abd2bb
payce06 <paycegrossman@gmail.com>
2024-12-07 19:26:09 -0500

    calculator-cpp

--- calculator.cpp
+++ calculator.cpp
@@ -24,3 +24,34 @@ public:
         return a / b;
     }
 };
+
+int main() {
+    Calculator calc;
+    double num1, num2;
+    char operation;
+
+    // Prompt user for input
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Perform calculation based on the operator
+    if (operation == '+') {
+        cout << "Result: " << calc.add(num1, num2) << endl;
+    } else if (operation == '-') {
+        cout << "Result: " << calc.subtract(num1, num2) << endl;
+    } else if (operation == '*') {
+        cout << "Result: " << calc.multiply(num1, num2) << endl;
+    } else if (operation == '/') {
+        cout << "Result: " << calc.divide(num1, num2) << endl;
+    } else {
+        cout << "Error: Invalid operator!" << endl;
+    }
+
+    return 0;
+}
